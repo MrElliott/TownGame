0cefbba14acae6867421624f7dbd0f7519c0a8ec
Elliott <elliottholt90@gmail.com>
2015-03-11 19:49:27 +0000

    added code to clear the console

--- Text_Town_Game/Text_Town_Game/main.cpp
+++ Text_Town_Game/Text_Town_Game/main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main() 
 {
 	int i;
+
 	cout << "Hello World!" << endl;
-	cout << "Start Input here";
+	cout << "Start Input here: ";
+
+	cin >> i;
+
+	system("cls");
 
 	cin >> i;
 
 	return 0;
 }
+
+void wait(int wait_time)
+{
+	long *start_time = new long;
+	*start_time = time(0);
+
+	while ((*start_time + wait_time) > time(0)){
+	};
+
+	delete start_time;
+};
